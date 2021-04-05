c677ed1f492795e47eadda433c26827afb710819
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-04-05 06:55:04 +0200

    [dist_py_module] Updated version, docs

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __author__ = 'Vladimir Roncevic'
 __copyright__ = 'Copyright 2017, https://vroncevic.github.io/dist_py_module'
 __credits__ = ['Vladimir Roncevic']
 __license__ = 'https://github.com/vroncevic/dist_py_module/blob/master/LICENSE'
-__version__ = '1.6.2'
+__version__ = '1.7.2'
 __maintainer__ = 'Vladimir Roncevic'
 __email__ = 'elektron.ronca@gmail.com'
 __status__ = 'Updated'
@@ -95,7 +95,7 @@ PYP_CLASSIFIERS = SUPPORTED_PY_VERSIONS + APPROVED_LICENSES
 
 setup(
     name='dist_py_module',
-    version='1.6.2',
+    version='1.7.2',
     description='Python package for generation of setup file',
     author='Vladimir Roncevic',
     author_email='elektron.ronca@gmail.com',
